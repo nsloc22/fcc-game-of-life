036aaf3b7dbd4a8e636eed8b70474ac0cd281056
komp <nsloc22@gmail.com>
2016-04-25 04:51:01 +0200

    ver.0.1.0

--- public/scripts/gol.js
+++ public/scripts/gol.js
@@ -1,107 +1,4 @@
-
-
-/*
-var cell = function(x, y, state) {
-  this.x = x;
-  this.y = y;
-  this.state = state ? state : false;
-  this.tempState = false;
-  this.update = function() {
-    var count = 0;
-    var x = this.x;
-    var y = this.y;
-    
-    arr.forEach(function(o) {
-      if(o.x === x-1 && o.y === y-1){ 
-        if(o.state) { count++; }
-      }
-      if(o.x === x && o.y === y-1){ 
-        if(o.state) { count++; }
-      }
-      if(o.x === x+1 && o.y === y-1){ 
-        if(o.state) { count++; }
-      }
-      if(o.x === x-1 && o.y === y){ 
-        if(o.state) { count++; }
-      }
-      if(o.x === x+1 && o.y === y){ 
-        if(o.state) { count++; }
-      }
-       if(o.x === x-1 && o.y === y+1){ 
-        if(o.state) { count++; }
-      }
-      if(o.x === x && o.y === y+1){ 
-        if(o.state) { count++; }
-      }
-      if(o.x === x+1 && o.y === y+1){ 
-        if(o.state) { count++; }
-      }  
-    });
-    switch(count){
-      case 2:
-        this.tempState = this.state ? true : false;
-        break;
-      case 3:
-        this.tempState = true;
-        break;
-      default:
-        this.tempState = false;
-    }
-  },
-    this.setState = function() { 
-      this.state = this.tempState;
-    }
-}
-
-var cell1 = new cell(0,0,true);
-var cell2 = new cell(1,0,true);
-var cell3 = new cell(2,0);
-var cell4 = new cell(0,1,true);
-var cell5 = new cell(1,1);
-var cell6 = new cell(2,1);
-var cell7 = new cell(0,2);
-var cell8 = new cell(1,2);
-var cell9 = new cell(2,2);
-var arr = [cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8,cell9];
-
-var pass = function() {
-  arr.forEach(function(o){
-    o.update();
-  });
-   arr.forEach(function(o){
-    o.setState();
-  });
-};
-
-var res = function() {
-  arr.forEach(function(o){
-    console.log(o.x);
-    console.log(o.y);
-    console.log(o.state);  
-  });
-};
-
-pass();
-//res();
-*/
-var COUNTERCELL = -1;
-var COUNTERLOGIC = 0;
-var CELLS = [];
-var Board = React.createClass({
-  render: function() {
-    var rows = [];
-    for(var i = 0; i < 10; i++){
-      rows.push(<BoardRow y={i} key={i} />);
-    }
-    return (
-      <table className="border">
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    );
-  }
-});
+var update = React.addons.update;
 
 var SetIntervalMixin = {
   componentWillMount: function() {
@@ -115,70 +12,124 @@ var SetIntervalMixin = {
   }
 };
 
-var BoardRow = React.createClass({ 
-  render: function() {
+var Board = React.createClass({
+  getInitialState: function() {
+    return {cells: [], running: false, cleared: false, generation: 0};
+  },
+  componentDidMount: function() {
+    this.setState({cells: this.randCells()});
+    this.onStart();
+  },
+  randCells: function() {
+    var cells = [];
+    var size = this.props.size;
+    for(var i = 0; i < size; i++){
+      for(var j = 0; j < size; j++){
+        var randomAlive = Math.random() > 0.8 ? true : false;
+        cells.push({x: j, y: i, alive: randomAlive, tempState: false, updateTemp: updateTemp});
+      }
+    }
+    return cells; 
+  },
+  mixins: [SetIntervalMixin],
+  onStart: function() {
+    if(this.state.cleared){
+      this.setState({cells: this.randCells(), cleared: false});
+      this.setupIntervals();
+      this.setState({running: true});
+    }
+    if(!this.state.running){
+      this.setupIntervals();
+      this.setState({running: true});
+    } 
+  },
+  onPause: function() {
+    this.intervals.forEach(clearInterval);
+    this.setState({running: false});
+  },
+  onClear: function() {
+    this.onPause();
+    this.state.cells.forEach(function(o){
+      o.alive = false;
+    });
+    this.setState({cells: this.state.cells, cleared: true, generation: 0});
+    
+  },
+  setupIntervals: function() {
+    this.setInterval(this.update, 100);
+  },
+  update: function() {
     var self = this;
-    var data = [];
-    for(var i = 0; i < 10; i++){ 
-      var randomAlive = Math.random() > 0.05 ? true : false;
-      var logicCell = (<LogicCell 
-                         key={COUNTERLOGIC} 
-                         id={COUNTERLOGIC} 
-                         alive={randomAlive}
-                         x={i} 
-                         y={this.props.y}
-                         />
-                      );
-      data.push(logicCell);
-      COUNTERLOGIC++;
+    this.state.cells.forEach(function(o){
+      o.updateTemp(o.x, o.y, self.state.cells);
+    });
+    this.state.cells.forEach(function(o) {
+      o.alive = o.tempState;
+    });
+    this.setState({cells: this.state.cells, generation: this.state.generation + 1});
+  },
+  onClicked: function(id) {
+    var cells = React.addons.update(this.state.cells, {
+    [id]: { alive: {$set: !this.state.cells[id].alive} }
+  });
+  this.setState({cells});
+  },
+  render: function() {
+    var rows = []; 
+    var size = this.props.size;
+    var counterCell = 0;
+    var counterRow = size + 1;
+    for(var i = 0; i < size; i++){
+      rows.push(<BoardRow key={Math.random()}/>);
+      counterRow++;
+        for(var j= 0; j < size; j++){
+          if(this.state.cells.length > 0){
+            rows.push(<Cell 
+                        id={counterCell} 
+                        key={counterCell} 
+                        alive={this.state.cells[counterCell].alive}
+                        onClicked={this.onClicked}/>);
+            counterCell++;
+          }
+        }
     }
     return (
-      <tr key={Math.random()}>
-        {data}
-      </tr>
+      <div>
+        <div className="page-header"><h2>Game of Life</h2></div>
+        <div className="board none">{rows}</div>
+        <div className="none">Generation: <span className="badge">{this.state.generation}</span></div><br />
+        <button className="start btn" onClick={this.onStart}>Start</button>
+        <button className="start btn" onClick={this.onPause}>Pause</button>
+        <button className="start btn" onClick={this.onClear}>Clear</button>
+        <div className="page-header"></div>
+      </div>
+    );
+  }
+}); 
+
+var BoardRow = React.createClass({
+  render: function() {
+    return (
+      <div className="boardRow"></div>
     );
   }
 });
 
-var LogicCell = React.createClass({
-  getInitialState: function (){
-    return {alive: this.props.alive, tempAlive: false};
-  },
+var Cell = React.createClass({
   onClicked: function() {
-    this.setState({alive: !this.state.alive});
-    //CELLS[this.props.id].tempState = !this.state.alive;
+    this.props.onClicked(this.props.id);
   },
-  mixins: [SetIntervalMixin],
-  componentDidMount: function() {
-     CELLS.push({id: this.props.id, alive: this.state.alive, x: this.props.x, y: this.props.y});
-     this.setInterval(this.update, 100);
-     this.setInterval(this.setAlive, 100);
-  },
-  update: function() {
-    var count = 0;
-    var x = this.props.x;
-    var y = this.props.y;
-    
-    // var xOffset = 0;
-    // var yOffset = 0;
-    // var len = CELLS.length;
-     
-//     if(x-1 < 0 ){
-//       xOffset = len;
-//     }
-//     if(x+1 === len){
-//       xOffset = -len;
-//     }
-    
-//     if(y-1 < 0){
-//       yOffset = len;
-//     }
-    
-//     if(y+1 === len){
-//       yOffset = -len;
-//     }
-    
-    CELLS.forEach(function(o) {
+  render: function() {
+    var style = this.props.alive ? {backgroundColor: 'red'} : null;
+    return (
+      <div className="cell" style={style} onClick={this.onClicked}></div>
+    );
+  }
+});
+
+var updateTemp = function(x,y,arr) {
+  var count = 0;
+  arr.forEach(function(o) {
       if(o.x === x-1 && o.y === y-1){ 
         if(o.alive) { count++; }
       }
@@ -210,54 +161,21 @@ var LogicCell = React.createClass({
       case 6:
       case 5:
       case 4:  
-        this.setState({alive: false});
-        CELLS[this.props.id].tempState = false;
+        this.tempState = false;
         break;
       case 3:
-        this.setState({alive: true});
-        CELLS[this.props.id].tempState = true;
+        this.tempState = true;
         break;
       case 2:
-        this.setState({alive: this.state.alive ? true : false});
-        CELLS[this.props.id].tempState = this.state.alive ? true : false;
+        this.tempState = this.alive ? true : false;
         break;
       case 1:
       case 0:
-        this.setState({alive: false});
-        CELLS[this.props.id].tempState = false;
+        this.tempState = false;
         break;
       default:
-        this.setState({alive: false});
-        CELLS[this.props.id].tempState = false;
-    }
-    COUNTERCELL = -1;
-   },
-  setAlive: function() { 
-    CELLS[this.props.id].alive = CELLS[this.props.id].tempState;
-  },
-  render: function() {
-    COUNTERCELL++;
-    return (
-      <Cell 
-        key={COUNTERCELL} 
-        id={COUNTERCELL}
-        x={this.props.x}
-        y={this.props.y}
-        alive={this.state.alive}
-        onClicked={this.onClicked}
-        />
-      );
-  }
-});
+        this.tempState = false;
+    } 
+};
 
-var Cell = React.createClass({
-  render: function() {
-    var style = this.props.alive ? {backgroundColor: 'red'} : null;
-    return (
-      <td className="sq" onClick={this.props.onClicked} style={style}>
-      </td>
-    );
-  }
-});
-
-ReactDOM.render(<Board />, document.getElementById('content'));
+ReactDOM.render(<Board size={30}/>, document.getElementById('content'));
